a543356b8b78ce42e31601927a36205d46c52c26
Kevin Hendry <kevin@lugnut.io>
2021-03-31 17:09:29 -0400

    Implement comment parsing and metric construction
    - Parses comment lines and uses data to add metrics to a temp registry
    - Once parsing completes, the regisry is used to output JSON formatted
      version of the data

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,4 +1,162 @@
+import { textToJSON } from '../index'
 
-describe('', () => {
-  it('', () => {})
+describe('textToJSON', () => {
+  it('simple validating test', async () => {
+    const input = `
+    # HELP test_counter_name test_counter_help
+    # TYPE test_counter_name counter
+    test_counter_name 13
+    
+    # HELP test_histogram_name test_histogram_help
+    # TYPE test_histogram_name histogram
+    test_histogram_name_bucket{le="0.005"} 0
+    test_histogram_name_bucket{le="0.01"} 0
+    test_histogram_name_bucket{le="0.025"} 0
+    test_histogram_name_bucket{le="0.05"} 0
+    test_histogram_name_bucket{le="0.1"} 0
+    test_histogram_name_bucket{le="0.25"} 0
+    test_histogram_name_bucket{le="0.5"} 0
+    test_histogram_name_bucket{le="1"} 0
+    test_histogram_name_bucket{le="2.5"} 0
+    test_histogram_name_bucket{le="5"} 0
+    test_histogram_name_bucket{le="10"} 1
+    test_histogram_name_bucket{le="+Inf"} 1
+    test_histogram_name_sum 10
+    test_histogram_name_count 1
+    
+    # HELP test_gauge_name test_gauge_help
+    # TYPE test_gauge_name gauge
+    test_gauge_name 123
+    
+    # HELP test_summary_name test_summary_help
+    # TYPE test_summary_name summary
+    test_summary_name{quantile="0.01"} 1234
+    test_summary_name{quantile="0.05"} 1234
+    test_summary_name{quantile="0.5"} 1234
+    test_summary_name{quantile="0.9"} 1234
+    test_summary_name{quantile="0.95"} 1234
+    test_summary_name{quantile="0.99"} 1234
+    test_summary_name{quantile="0.999"} 1234
+    test_summary_name_sum 1234
+    test_summary_name_count 1    
+    `
+    const result = await textToJSON({ metrics: input })
+    const expected = [{
+      help: 'test_counter_help',
+      name: 'test_counter_name',
+      type: 'counter',
+      values: [{
+        value: 13,
+        labels: {}
+      }],
+      aggregator: 'sum'
+    }, {
+      name: 'test_histogram_name',
+      help: 'test_histogram_help',
+      type: 'histogram',
+      values: [{
+        labels: { le: 0.005 },
+        value: 0,
+        metricName: 'test_histogram_name_bucket'
+      }, {
+        labels: { le: 0.01 },
+        value: 0,
+        metricName: 'test_histogram_name_bucket'
+      }, {
+        labels: { le: 0.025 },
+        value: 0,
+        metricName: 'test_histogram_name_bucket'
+      }, {
+        labels: { le: 0.05 },
+        value: 0,
+        metricName: 'test_histogram_name_bucket'
+      }, {
+        labels: { le: 0.1 },
+        value: 0,
+        metricName: 'test_histogram_name_bucket'
+      }, {
+        labels: { le: 0.25 },
+        value: 0,
+        metricName: 'test_histogram_name_bucket'
+      }, {
+        labels: { le: 0.5 },
+        value: 0,
+        metricName: 'test_histogram_name_bucket'
+      }, {
+        labels: { le: 1 },
+        value: 0,
+        metricName: 'test_histogram_name_bucket'
+      }, {
+        labels: { le: 2.5 },
+        value: 0,
+        metricName: 'test_histogram_name_bucket'
+      }, {
+        labels: { le: 5 },
+        value: 0,
+        metricName: 'test_histogram_name_bucket'
+      }, {
+        labels: { le: 10 },
+        value: 1,
+        metricName: 'test_histogram_name_bucket'
+      }, {
+        labels: { le: '+Inf' },
+        value: 1,
+        metricName: 'test_histogram_name_bucket'
+      }, {
+        labels: { },
+        value: 10,
+        metricName: 'test_histogram_name_sum'
+      }, {
+        labels: { },
+        value: 1,
+        metricName: 'test_histogram_name_count'
+      }],
+      aggregator: 'sum'
+    }, {
+      help: 'test_gauge_help',
+      name: 'test_gauge_name',
+      type: 'gauge',
+      values: [{
+        value: 123,
+        labels: { }
+      }],
+      aggregator: 'sum'
+    }, {
+      name: 'test_summary_name',
+      help: 'test_summary_help',
+      type: 'summary',
+      values: [{
+        labels: { quantile: 0.01 },
+        value: 1234
+      }, {
+        labels: { quantile: 0.05 },
+        value: 1234
+      }, {
+        labels: { quantile: 0.5 },
+        value: 1234
+      }, {
+        labels: { quantile: 0.9 },
+        value: 1234
+      }, {
+        labels: { quantile: 0.95 },
+        value: 1234
+      }, {
+        labels: { quantile: 0.99 },
+        value: 1234
+      }, {
+        labels: { quantile: 0.999 },
+        value: 1234
+      }, {
+        metricName: 'test_summary_name_sum',
+        labels: { },
+        value: 1234
+      }, {
+        metricName: 'test_summary_name_count',
+        labels: { },
+        value: 1
+      }],
+      aggregator: 'sum'
+    }]
+    expect(result).toStrictEqual(expected)
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,141 @@
-console.log('hi')
+import { Registry, Counter, Gauge, Summary, Histogram } from 'prom-client'
+
+import * as readline from 'readline'
+import { Readable } from 'stream'
+
+
+const typeToCtorMap = {
+  counter: Counter,
+  histogram: Histogram,
+  gauge: Gauge,
+  summary: Summary
+}
+/**
+ * Convert default Prometheus text formatted metrics data into JSON
+ *
+ * @param {Object} options - object defining the required and supported options for this function
+ * @param {[string] | string | ReadableStream} metrics - metrics data to be converted to JSON
+ * @returns {Object} JSON array of the provided metrics
+ */
+export async function textToJSON ({ metrics }: { metrics: [string] | string | Readable }) {
+  const registry = new Registry()
+  if (typeof metrics === 'string' || Array.isArray(metrics)) {
+    metrics = Readable.from(metrics)
+  }
+  /*
+   * Parse the string data into metrics and add them to the registry
+   */
+  const rl = readline.createInterface({ input: metrics })
+  let metric: any = {}
+  const read = async () => {
+    return new Promise((resolve, reject) => {
+      rl.on('line', line => {
+        if (line.length === 0) { return }
+        console.log(`${line}`)
+        readLine({ line, metric })
+        if (metric.ctor) {
+          const { ctor: Ctor, name, help } = metric
+          // Do things!
+          // eslint-disable-next-line no-new
+          new Ctor({
+            name,
+            help,
+            registers: [registry]
+          })
+          metric = {}
+        }
+      })
+      rl.on('close', () => {
+        resolve('')
+      })
+    })
+  }
+  await read()
+  /*
+   * Serialize the registries metrics into JSON
+   */
+  return registry.getMetricsAsJSON()
+}
+
+// function readHelpLine ({ help, registry }: { help: string, registry: Registry }) {
+// }
+// function readTypeLine () {
+// }
+
+function readCommentLine ({ commentLine, metric }: { commentLine: string, metric: any }): void {
+  const tokenizedCommentLine = commentLine.split(' ')
+  const [, keyword, metricName, ...rest] = tokenizedCommentLine
+  switch (keyword) {
+    case 'HELP': {
+      const helpText = rest.join('')
+      metric.name = metricName
+      metric.help = helpText
+      return
+    }
+    case 'TYPE': {
+      const type = rest.join('')
+      metric.ctor = typeToCtorMap[type]
+      // eslint-disable-next-line no-useless-return
+      return
+    }
+  }
+}
+
+function readMetricLine ({ metricLine, metric }: { metricLine: string, metric: any }) {
+
+}
+
+function readMetricName () {}
+function readLabels () {}
+function readLabelName () {}
+function readLabelValue () {}
+
+function readLine ({ line, metric }: { line: string, metric: any }) {
+  /*
+   * Find 1st non-blank character
+   */
+  const arr = line.split('')
+  for (let i = 0; i < arr.length; i += 1) {
+    const char = arr[i]
+    if (char !== ' ' && char !== '\t') {
+      if (char === '#') {
+        readCommentLine({ commentLine: line.substring(i), metric })
+      } else {
+        readMetricLine({ metricLine: line.substring(i), metric })
+      }
+    }
+  }
+}
+
+/*
+ * <HACK>
+ * Testing the metric APIs and produced JSON
+ */
+// const counter = new Counter({
+//   name: 'test_counter_name',
+//   help: 'test_counter_help',
+//   registers: [registry]
+// })
+// counter.inc(13)
+// const histogram = new Histogram({
+//   name: 'test_histogram_name',
+//   help: 'test_histogram_help',
+//   registers: [registry]
+// })
+// histogram.observe(10)
+// const gauge = new Gauge({
+//   name: 'test_gauge_name',
+//   help: 'test_gauge_help',
+//   registers: [registry]
+// })
+// gauge.set(123)
+// const summary = new Summary({
+//   name: 'test_summary_name',
+//   help: 'test_summary_help',
+//   registers: [registry]
+// })
+// summary.observe(1234)
+/*
+  * </HACK>
+  */
+// console.log(await registry.metrics())
